1e7975a1fff7a512bd4617eeb3b843479bf917a3
S0UL <93375971+AlexanderTsik@users.noreply.github.com>
2025-02-07 20:16:43 +0400

    completed chat and custom click tests

---
+++ sample-app/src/main/java/com/atiurin/sampleapp/data/constants/TestConstants.kt
@@ -0,0 +1,8 @@
+package com.atiurin.sampleapp.constants
+
+object TestConstants {
+    const val FRIEND_NAME = "Chandler Bing"
+    const val GREETING_MESSAGE = "Hi Chandler, hope you're doing well!"
+    const val CUSTOM_CLICKS_TEXT = "Custom Clicks"
+    const val WAIT_TIMEOUT = 3L
+}
